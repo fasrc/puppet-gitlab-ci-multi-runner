3cc319577df23fd81f09153eb02ce38360a8da41
Justin Riley <justin.t.riley@gmail.com>
2016-02-26 15:54:28 -0500

    install gitlab-ci-multi-runner package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,17 @@
-
-
 class gitlab_ci_multi_runner (
   $include_srepo          = true,
   $manage_repo            = true,
   $package_repo_location  = gitlab_ci_multi_runner::params::package_repo_location,
   $package_repo_gpgkey    = gitlab_ci_multi_runner::params::package_repo_gpgkey,
   $package_srepo_location = gitlab_ci_multi_runner::params::package_srepo_location,
+  $version                = gitlab_ci_multi_runner::params::version,
 ){
 
   class { 'gitlab_ci_multi_runner::repo': }
   
   contain 'gitlab_ci_multi_runner::repo'
 
+  package { 'gitlab-ci-multi-runner':
+      ensure => $version,
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,23 +3,24 @@
 # Configuration of gitlab-ci multi runner
 #
 class gitlab_ci_multi_runner::params {
-  
+
+  $version = 'latest',
   $package_repo_gpgkey = https://packages.gitlab.com/gpg.key
-  
+
   case $::osfamily {
     'RedHat': {
-      
+
       if $::operatingsystem == 'Fedora' {
         $package_repo_location = "https://packages.gitlab.com/runner/gitlab-ci-multi-runner/fedora/${::operatingsystemmajrelease}/\$basearch"
         $package_srepo_location = "https://packages.gitlab.com/runner/gitlab-ci-multi-runner/fedora/${::operatingsystemmajrelease}/SRPMS"
-      } else{ 
+      } else{
         $package_repo_location = "https://packages.gitlab.com/runner/gitlab-ci-multi-runner/el/${::operatingsystemmajrelease}/\$basearch"
         $package_srepo_location = "https://packages.gitlab.com/runner/gitlab-ci-multi-runner/el/${::operatingsystemmajrelease}/SRPMS"
       }
     }
-    
+
     'Debian': {
-      
+
       if $::operatingsystem == 'Debian' {
         $package_repo_location = "https://packages.gitlab.com/runner/gitlab-ci-multi-runner/debian/"
         $package_srepo_location = undef

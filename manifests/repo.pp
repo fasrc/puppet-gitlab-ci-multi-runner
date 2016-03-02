03eb0216cbed8e3d9e2dc972b23942c93c27e152
Justin Riley <justin.t.riley@gmail.com>
2016-03-02 13:47:38 -0500

    use 0/1 instead of true/false for gpgcheck params

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -9,16 +9,16 @@ class gitlab_ci_multi_runner::repo {
         yumrepo {'gitlab-ci-multi-runner':
           baseurl  => $gitlab_ci_multi_runner::package_repo_location,
           gpgkey   => $gitlab_ci_multi_runner::package_repo_gpgkey,
-          gpgcheck => false,
-          repo_gpgcheck => true,
+          gpgcheck => 0,
+          repo_gpgcheck => 1,
         }
 
         if ($gitlab_ci_multi_runner::include_srepo) {
           yumrepo {'gitlab-ci-multi-runner-source':
             baseurl  => $gitlab_ci_multi_runner::package_srepo_location,
             gpgkey   => $gitlab_ci_multi_runner::package_repo_gpgkey,
-            gpgcheck => false,
-            repo_gpgcheck => true,
+            gpgcheck => 0,
+            repo_gpgcheck => 1,
           }
         }
       }

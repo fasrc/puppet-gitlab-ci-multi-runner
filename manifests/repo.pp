419d3bc0ad5e50cd77d02c260df736fc31b097da
Justin Riley <justin.t.riley@gmail.com>
2016-03-02 13:58:26 -0500

    add description to yum repos

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,21 +3,26 @@
 #
 class gitlab_ci_multi_runner::repo {
 
+  $repo_name = 'gitlab-ci-multi-runner'
+  $src_repo_name = "${repo_name}-source"
+
   case $::osfamily {
     'RedHat': {
       if $gitlab_ci_multi_runner::manage_repo {
-        yumrepo {'gitlab-ci-multi-runner':
-          baseurl  => $gitlab_ci_multi_runner::package_repo_location,
-          gpgkey   => $gitlab_ci_multi_runner::package_repo_gpgkey,
-          gpgcheck => 0,
+        yumrepo { $repo_name:
+          desc          => $repo_name,
+          baseurl       => $gitlab_ci_multi_runner::package_repo_location,
+          gpgkey        => $gitlab_ci_multi_runner::package_repo_gpgkey,
+          gpgcheck      => 0,
           repo_gpgcheck => 1,
         }
 
         if ($gitlab_ci_multi_runner::include_srepo) {
-          yumrepo {'gitlab-ci-multi-runner-source':
-            baseurl  => $gitlab_ci_multi_runner::package_srepo_location,
-            gpgkey   => $gitlab_ci_multi_runner::package_repo_gpgkey,
-            gpgcheck => 0,
+          yumrepo { $src_repo_name:
+            desc          => $src_repo_name,
+            baseurl       => $gitlab_ci_multi_runner::package_srepo_location,
+            gpgkey        => $gitlab_ci_multi_runner::package_repo_gpgkey,
+            gpgcheck      => 0,
             repo_gpgcheck => 1,
           }
         }
@@ -29,7 +34,6 @@ class gitlab_ci_multi_runner::repo {
         include ::apt
         # apt-transport-https is required by apt to get the source
         ensure_packages(['apt-transport-https'])
-
       }
     }
     default: {

82653c8ea85637b694c528d0c8a01e9fd516eba7
Dan Kelleher <dkelleher@g.harvard.edu>
2017-03-01 16:30:05 -0500

    Add osfamily facts and fix rspec version
    The osfamily facts were causing travis to fail.  Additionally rspec
    3.1.0 is necessary for puppet 3.8.7.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,9 @@
 source 'https://rubygems.org'
 
 puppetversion = ENV.key?('PUPPET_GEM_VERSION') ? "= #{ENV['PUPPET_GEM_VERSION']}" : ['>= 3.3']
+rspecversion = ENV.key?('RSPEC_GEM_VERSION') ? "= #{ENV['RSPEC_GEM_VERSION']}" : ['>=3.2.0']
 gem 'puppet', puppetversion
+gem 'rspec', rspecversion
 gem 'puppetlabs_spec_helper', '>= 0.1.0'
 gem 'puppet-lint', '>= 0.3.2'
 gem 'puppet-syntax', '>=2.2.0'
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,6 +7,13 @@ describe 'gitlab_ci_multi_runner', :type => :class do
     context "on #{osfamily}" do
 
       if osfamily == 'RedHat'
+        let(:facts) { {
+          :osfamily                  => osfamily,
+          :operatingsystem           => 'RedHat',
+          :operatingsystemrelease    => '7.2',
+          :operatingsystemmajrelease => '7',
+          :kernelversion             => '2.6.32'
+        } }
 
         it { should compile.with_all_deps }
 

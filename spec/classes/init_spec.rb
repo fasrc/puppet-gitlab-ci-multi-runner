ee7bebf07d4f2bf323f72ca86817375aaa655a4c
Justin Riley <justin.t.riley@gmail.com>
2017-03-08 21:34:18 -0500

    tests now at 100% coverage

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -17,6 +17,19 @@ describe 'gitlab_ci_multi_runner', :type => :class do
       it { should contain_class('gitlab_ci_multi_runner::config').that_requires('Class[gitlab_ci_multi_runner::install]') }
       it { should contain_class('gitlab_ci_multi_runner::service').that_subscribes_to('Class[gitlab_ci_multi_runner::config]') }
 
+      case osfamily
+      when :RedHat
+        it { should contain_yumrepo('gitlab-ci-multi-runner') }
+        it { should contain_yumrepo('gitlab-ci-multi-runner-source') }
+      when :Ubuntu
+        it { should contain_class('apt') }
+        it { should contain_package('apt-transport-https') }
+      end
+
+      it { should contain_package('gitlab-ci-multi-runner') }
+      it { should contain_service('gitlab-runner') }
+      it { should contain_concat('/etc/gitlab-runner/config.toml') }
+      it { should contain_concat__fragment('gitlab_ci_multi_runner_globals') }
     end
   end
 end
